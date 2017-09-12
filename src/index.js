ab3b94ad44a32507f21a6cd0b555ef17f32afd96
James Bunt <james.bunt@vendhq.com>
2017-09-13 10:08:34 +1200

    Initialise redux with empty state

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './components/App'
+import reducer from './reducers'
 import registerServiceWorker from './registerServiceWorker'
 
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
 ReactDOM.render(
-  <BrowserRouter><App /></BrowserRouter>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 )
 registerServiceWorker()
