3c9e6ce5dcba658977b5347c242e6a11bc0492ac
James Bunt <james.bunt@vendhq.com>
2017-09-13 11:15:48 +1200

    Plug in redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import { createStore } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 import './index.css'
 import App from './components/App'
 import reducer from './reducers'
 import registerServiceWorker from './registerServiceWorker'
 
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <BrowserRouter>
