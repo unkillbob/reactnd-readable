02881020725d25c9d5b9f9783e225a8e3b40ec37
James Bunt <james.bunt@vendhq.com>
2017-10-02 13:03:32 +1300

    Plan reducer unit tests, test UPDATE_CATEGORIES

--- src/reducers/reducers.test.js
+++ src/reducers/reducers.test.js
@@ -1,3 +1,16 @@
+import {
+  RECEIVE_CATEGORIES,
+  RECEIVE_COMMENTS,
+  RECEIVE_UPDATED_COMMENT,
+  COMMENT_DELETED,
+  RECEIVE_POSTS,
+  RECEIVE_POST,
+  RECEIVE_UPDATED_POST,
+  POST_DELETED,
+  UPDATE_CATEGORY,
+  UPDATE_SORT_BY,
+  UPDATE_SORT_COMMENTS_BY
+} from '../actions'
 import reducer from './'
 
 describe('default state', () => {
@@ -13,4 +26,95 @@ describe('default state', () => {
     expect(state).toHaveProperty('sortBy', 'voteScore')
     expect(state).toHaveProperty('sortCommentsBy', 'voteScore')
   })
+})
+
+describe('RECEIVE_CATEGORIES', () => {
+  it('should update the list of categories', () => {
+    const state = { categories: [{ name: 'foo', path: 'foo' }] }
+    const categories = [
+      { name: 'bar', path: 'bar' },
+      { name: 'baz', path: 'baz' }
+    ]
+    const updatedState = reducer(state, {
+      type: RECEIVE_CATEGORIES,
+      categories
+    })
+    expect(updatedState).toHaveProperty('categories', categories)
+  })
+})
+
+describe('RECEIVE_POSTS', () => {
+  it('should update the list of posts')
+
+  it('should filter out deleted posts')
+})
+
+describe('RECEIVE_POST', () => {
+  it('should update the active post')
+})
+
+describe('RECEIVE_UPDATED_POST', () => {
+  it('should update the active post if it matches by ID')
+
+  it('should not update the active post if it does not match by ID')
+
+  it(
+    'should update the post in the posts collection if it is present in the collection'
+  )
+
+  it(
+    'should not update the post in the posts collection if it is not present in the collection'
+  )
+})
+
+describe('POST_DELETED', () => {
+  it('should set the active post to null if it matches by ID')
+
+  it('should not update the active post if it does not match by ID')
+
+  it(
+    'should remove the post from the posts collection if it is present in the collection'
+  )
+
+  it(
+    'should not change the posts collection if it is not present in the collection'
+  )
+})
+
+describe('RECEIVE_COMMENTS', () => {
+  it('should update the list of comments')
+})
+
+describe('RECEIVE_UPDATED_COMMENT', () => {
+  it(
+    'should update the comment in the comments collection if it is present in the collection'
+  )
+
+  it(
+    'should add the comment to the comments collection if it is not present in the collection'
+  )
+})
+
+describe('COMMENT_DELETED', () => {
+  it(
+    'should remove the comment from the comments collection if it is present in the collection'
+  )
+
+  it(
+    'should not change the comments collection if it is not present in the collection'
+  )
+})
+
+describe('UPDATE_CATEGORY', () => {
+  it('should update the active category')
+
+  it('should allow updating the active category to null')
+})
+
+describe('UPDATE_SORT_BY', () => {
+  it('should update the sortBy')
+})
+
+describe('UPDATE_SORT_COMMENTS_BY', () => {
+  it('should update the sortCommentsBy')
 })
