ba98bef8d4e7d66029e4ac33fc4a353a3919c7fb
James Bunt <james.bunt@vendhq.com>
2017-10-02 16:10:26 +1300

    Unit test fetchPost, createPost and createComment

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -2,16 +2,24 @@ import API from '../utils/api'
 import {
   fetchCategories,
   fetchPosts,
+  fetchPost,
+  createPost,
   fetchComments,
+  createComment,
   RECEIVE_CATEGORIES,
   RECEIVE_POSTS,
-  RECEIVE_COMMENTS
+  RECEIVE_POST,
+  RECEIVE_COMMENTS,
+  RECEIVE_UPDATED_COMMENT
 } from './'
 
 jest.mock('../utils/api', () => {
   return {
     fetchCategories: jest.fn(() => Promise.resolve([])),
     fetchPosts: jest.fn(() => Promise.resolve([])),
+    fetchPost: jest.fn(() => Promise.resolve()),
+    createPost: jest.fn(() => Promise.resolve()),
+    createComment: jest.fn(() => Promise.resolve()),
     fetchComments: jest.fn(() => Promise.resolve([]))
   }
 })
@@ -79,17 +87,61 @@ describe('fetchPosts', () => {
 })
 
 describe('fetchPost', () => {
-  it('should fetch the post with the given ID from the API')
+  afterEach(() => {
+    API.fetchPost.mockClear()
+  })
 
-  it('should dispatch the receivePost action with the post returned by the API')
+  it('should fetch the post with the given ID from the API', () => {
+    const postId = 123
+    fetchPost(postId)(dispatch)
+
+    expect(API.fetchPost).toHaveBeenCalledTimes(1)
+    expect(API.fetchPost).toHaveBeenCalledWith(postId)
+  })
+
+  it('should dispatch the RECEIVE_POST action with the post returned by the API', () => {
+    const post = { id: 13 }
+    API.fetchPost.mockImplementationOnce(() => Promise.resolve(post))
+
+    return fetchPost(post.i)(dispatch).then(() => {
+      expect(dispatch).toHaveBeenCalledWith({
+        type: RECEIVE_POST,
+        post
+      })
+    })
+  })
 })
 
 describe('createPost', () => {
-  it('should create a new post with the given data via the API')
+  afterEach(() => {
+    API.createPost.mockClear()
+  })
 
-  it(
-    'should dispatch the receivePost action with the created post returned by the API'
-  )
+  it('should create a new post with the given data via the API', () => {
+    const postDetails = { id: 13, title: 'foo' }
+    createPost(postDetails)(dispatch)
+
+    expect(API.createPost).toHaveBeenCalledTimes(1)
+    expect(API.createPost).toHaveBeenCalledWith(postDetails)
+  })
+
+  it('should dispatch the RECEIVE_POST action with the created post returned by the API', () => {
+    const createdPost = {
+      id: 13,
+      title: 'foo',
+      timestamp: Date.now(),
+      voteScore: 1
+    }
+
+    API.createPost.mockImplementationOnce(() => Promise.resolve(createdPost))
+
+    return createPost({ id: 13, title: 'foo' })(dispatch).then(() => {
+      expect(dispatch).toHaveBeenCalledWith({
+        type: RECEIVE_POST,
+        post: createdPost
+      })
+    })
+  })
 })
 
 describe('updatePost', () => {
@@ -135,7 +187,7 @@ describe('fetchComments', () => {
     const comments = [{ id: 1 }, { id: 3 }]
     API.fetchComments.mockImplementationOnce(() => Promise.resolve(comments))
 
-    return fetchComments()(dispatch).then(() => {
+    return fetchComments(123)(dispatch).then(() => {
       expect(dispatch).toHaveBeenCalledWith({
         type: RECEIVE_COMMENTS,
         comments
@@ -145,11 +197,37 @@ describe('fetchComments', () => {
 })
 
 describe('createComment', () => {
-  it('should create a new comment with the given data via the API')
+  afterEach(() => {
+    API.createComment.mockClear()
+  })
 
-  it(
-    'should dispatch the receiveUpdatedComment action with the created comment returned by the API'
-  )
+  it('should create a new comment with the given data via the API', () => {
+    const commentDetails = { id: 13, body: 'foo' }
+    createComment(commentDetails)(dispatch)
+
+    expect(API.createComment).toHaveBeenCalledTimes(1)
+    expect(API.createComment).toHaveBeenCalledWith(commentDetails)
+  })
+
+  it('should dispatch the RECEIVE_UPDATED_COMMENT action with the created comment returned by the API', () => {
+    const createdComment = {
+      id: 13,
+      body: 'foo',
+      timestamp: Date.now(),
+      voteScore: 1
+    }
+
+    API.createComment.mockImplementationOnce(() =>
+      Promise.resolve(createdComment)
+    )
+
+    return createComment({ id: 13, body: 'foo' })(dispatch).then(() => {
+      expect(dispatch).toHaveBeenCalledWith({
+        type: RECEIVE_UPDATED_COMMENT,
+        comment: createdComment
+      })
+    })
+  })
 })
 
 describe('updateComment', () => {
