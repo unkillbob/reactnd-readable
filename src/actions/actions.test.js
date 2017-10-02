4713004ace42c693326ae54e9cbe17cf90d83db9
James Bunt <james.bunt@vendhq.com>
2017-10-02 15:58:41 +1300

    Unit test fetchCategories, fetchPosts and fetchComments

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -1,21 +1,30 @@
 import API from '../utils/api'
-import { fetchCategories, RECEIVE_CATEGORIES } from './'
+import {
+  fetchCategories,
+  fetchPosts,
+  fetchComments,
+  RECEIVE_CATEGORIES,
+  RECEIVE_POSTS,
+  RECEIVE_COMMENTS
+} from './'
 
 jest.mock('../utils/api', () => {
   return {
-    fetchCategories: jest.fn(() => Promise.resolve([]))
+    fetchCategories: jest.fn(() => Promise.resolve([])),
+    fetchPosts: jest.fn(() => Promise.resolve([])),
+    fetchComments: jest.fn(() => Promise.resolve([]))
   }
 })
 
 const dispatch = jest.fn()
 
 afterEach(() => {
-  dispatch.mockReset()
+  dispatch.mockClear()
 })
 
 describe('fetchCategories', () => {
   afterEach(() => {
-    API.fetchCategories.mockReset()
+    API.fetchCategories.mockClear()
   })
 
   it('should fetch categories from the API', () => {
@@ -23,7 +32,7 @@ describe('fetchCategories', () => {
     expect(API.fetchCategories).toHaveBeenCalledTimes(1)
   })
 
-  it('should dispatch the receiveCategories action with the categories returned by the API', () => {
+  it('should dispatch the RECEIVE_CATEGORIES action with the categories returned by the API', () => {
     const categories = [{ name: 'foo' }, { name: 'bar' }]
     API.fetchCategories.mockImplementationOnce(() =>
       Promise.resolve(categories)
@@ -39,13 +48,34 @@ describe('fetchCategories', () => {
 })
 
 describe('fetchPosts', () => {
-  it('should fetch posts from the API')
+  afterEach(() => {
+    API.fetchPosts.mockClear()
+  })
 
-  it(
-    'should dispatch the receivePosts action with the posts returned by the API'
-  )
+  it('should fetch posts from the API', () => {
+    fetchPosts()(dispatch)
+    expect(API.fetchPosts).toHaveBeenCalledTimes(1)
+  })
 
-  it('should fetch posts with the given category from the API')
+  it('should fetch posts with the given category from the API', () => {
+    const category = 'foo'
+    fetchPosts(category)(dispatch)
+
+    expect(API.fetchPosts).toHaveBeenCalledTimes(1)
+    expect(API.fetchPosts).toHaveBeenCalledWith(category)
+  })
+
+  it('should dispatch the RECEIVE_POSTS action with the posts returned by the API', () => {
+    const posts = [{ id: 1 }, { id: 3 }]
+    API.fetchPosts.mockImplementationOnce(() => Promise.resolve(posts))
+
+    return fetchPosts()(dispatch).then(() => {
+      expect(dispatch).toHaveBeenCalledWith({
+        type: RECEIVE_POSTS,
+        posts
+      })
+    })
+  })
 })
 
 describe('fetchPost', () => {
@@ -89,11 +119,29 @@ describe('voteForPost', () => {
 })
 
 describe('fetchComments', () => {
-  it('should fetch comments with the given post ID from the API')
+  afterEach(() => {
+    API.fetchComments.mockClear()
+  })
 
-  it(
-    'should dispatch the receiveComments action with the comments returned by the API'
-  )
+  it('should fetch comments with the given post ID from the API', () => {
+    const postId = 123
+    fetchComments(postId)(dispatch)
+
+    expect(API.fetchComments).toHaveBeenCalledTimes(1)
+    expect(API.fetchComments).toHaveBeenCalledWith(postId)
+  })
+
+  it('should dispatch the RECEIVE_COMMENTS action with the comments returned by the API', () => {
+    const comments = [{ id: 1 }, { id: 3 }]
+    API.fetchComments.mockImplementationOnce(() => Promise.resolve(comments))
+
+    return fetchComments()(dispatch).then(() => {
+      expect(dispatch).toHaveBeenCalledWith({
+        type: RECEIVE_COMMENTS,
+        comments
+      })
+    })
+  })
 })
 
 describe('createComment', () => {
