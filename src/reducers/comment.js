ddc7bcf96e542ff20906ea20fbc88ee772297243
James Bunt <james.bunt@vendhq.com>
2017-10-04 10:53:22 +1300

    New reducers and respective unit tests for category, post, comment

---
+++ src/reducers/comment.js
@@ -0,0 +1,40 @@
+import {
+  RECEIVE_COMMENTS,
+  RECEIVE_UPDATED_COMMENT,
+  COMMENT_DELETED,
+  UPDATE_SORT_COMMENTS_BY
+} from '../actions'
+
+const INITIAL_STATE = {
+  list: [],
+  sortBy: 'voteScore'
+}
+
+export default function reducer (state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case RECEIVE_COMMENTS:
+      return {
+        ...state,
+        list: action.comments
+      }
+    case RECEIVE_UPDATED_COMMENT:
+      return {
+        ...state,
+        list: state.list
+          .filter(c => c.id !== action.comment.id)
+          .concat(action.comment)
+      }
+    case COMMENT_DELETED:
+      return {
+        ...state,
+        list: state.list.filter(comment => comment.id !== action.comment.id)
+      }
+    case UPDATE_SORT_COMMENTS_BY:
+      return {
+        ...state,
+        sortBy: action.sortCommentsBy
+      }
+    default:
+      return state
+  }
+}
