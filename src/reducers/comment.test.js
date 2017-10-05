1f8329d5c5431648113a4fd792b39a9a2efe25e8
James Bunt <james.bunt@vendhq.com>
2017-10-06 12:10:23 +1300

    Update comment reducer unit tests

--- src/reducers/comment.test.js
+++ src/reducers/comment.test.js
@@ -9,7 +9,7 @@ import reducer from './comment'
 describe('default state', () => {
   it('should default the collection of comments to an empty object', () => {
     const state = reducer(undefined, { type: '' })
-    expect(state).toHaveProperty('byPostId', {})
+    expect(state).toHaveProperty('byId', {})
   })
 
   it('should default sortBy to "voteScore"', () => {
@@ -18,29 +18,32 @@ describe('default state', () => {
   })
 })
 
-const postId = 'post1'
 const comments = [{ id: 10 }, { id: 12 }, { id: 13 }]
-const byPostId = {
-  [postId]: comments
+const byId = {
+  10: comments[0],
+  12: comments[1],
+  13: comments[2]
 }
 const sortBy = 'voteScore'
 
 const state = {
-  byPostId,
+  byId,
   sortBy
 }
 
 describe('RECEIVE_COMMENTS', () => {
-  it('should update the list of comments for that postId', () => {
+  it('should update the collection of comments with the received comments', () => {
     const receivedComments = [{ id: 11 }, { id: 13 }, { id: 14 }]
     const updatedState = reducer(state, {
       type: RECEIVE_COMMENTS,
-      comments: receivedComments,
-      postId
+      comments: receivedComments
     })
     expect(updatedState).toEqual({
-      byPostId: {
-        [postId]: receivedComments
+      byId: {
+        ...byId,
+        11: receivedComments[0],
+        13: receivedComments[1],
+        14: receivedComments[2]
       },
       sortBy
     })
@@ -48,37 +51,37 @@ describe('RECEIVE_COMMENTS', () => {
 })
 
 describe('RECEIVE_UPDATED_COMMENT', () => {
-  it('should update the comment in the list of comments for the relevant post ID if it is present', () => {
+  it('should update the comment in the collection of comments if it was already present', () => {
     const updatedComment = {
       id: comments[1].id,
-      body: 'Many updates. Wow.',
-      parentId: postId
+      body: 'Many updates. Wow.'
     }
     const updatedState = reducer(state, {
       type: RECEIVE_UPDATED_COMMENT,
       comment: updatedComment
     })
     expect(updatedState).toEqual({
-      byPostId: {
-        [postId]: [comments[0], comments[2], updatedComment]
+      byId: {
+        ...byId,
+        [updatedComment.id]: updatedComment
       },
       sortBy
     })
   })
 
-  it('should add the comment to the list of comments for the relevant post ID if it is not present', () => {
+  it('should add the comment to the collections of comments if it was not present', () => {
     const updatedComment = {
       id: 17,
-      body: 'Totally new comment.',
-      parentId: postId
+      body: 'Totally new comment.'
     }
     const updatedState = reducer(state, {
       type: RECEIVE_UPDATED_COMMENT,
       comment: updatedComment
     })
     expect(updatedState).toEqual({
-      byPostId: {
-        [postId]: comments.concat(updatedComment)
+      byId: {
+        ...byId,
+        [updatedComment.id]: updatedComment
       },
       sortBy
     })
@@ -86,34 +89,32 @@ describe('RECEIVE_UPDATED_COMMENT', () => {
 })
 
 describe('COMMENT_DELETED', () => {
-  it('should remove the comment from the list of comments for the relevant post ID if it is present', () => {
+  it('should remove the comment from the collection of comments if it is present', () => {
     const deletedComment = {
-      id: comments[1].id,
-      parentId: postId
+      id: comments[1].id
     }
     const updatedState = reducer(state, {
       type: COMMENT_DELETED,
       comment: deletedComment
     })
+
+    const expectedComments = { ...byId }
+    delete expectedComments[deletedComment.id]
+
     expect(updatedState).toEqual({
-      byPostId: {
-        [postId]: [comments[0], comments[2]]
-      },
+      byId: expectedComments,
       sortBy
     })
   })
 
-  it('should not change the list of comments for the relevant post ID if it is not present', () => {
-    const deletedComment = {
-      id: 17,
-      parentId: postId
-    }
+  it('should not change the collection of comments if it is not present', () => {
+    const deletedComment = { id: 17 }
     const updatedState = reducer(state, {
       type: COMMENT_DELETED,
       comment: deletedComment
     })
     expect(updatedState).toEqual({
-      byPostId,
+      byId,
       sortBy
     })
   })
@@ -127,7 +128,7 @@ describe('UPDATE_SORT_COMMENTS_BY', () => {
       sortBy: updatedSortCommentsBy
     })
     expect(updatedState).toEqual({
-      byPostId,
+      byId,
       sortBy: updatedSortCommentsBy
     })
   })
